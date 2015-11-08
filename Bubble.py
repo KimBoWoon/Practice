ad91cf5be9968d4c961aee7aff95d7000adfa86e
KimBoWoon <haioer@naver.com>
2015-11-08 22:23:49 +0900

    수정

--- Bubble.py
+++ Bubble.py
@@ -1,11 +1,12 @@
 __author__ = '보운'
 
 sortList = input().split()
+sortList = [99, 81, 700, 6, 5005, 444, 321, 23456, 1]
 size = len(sortList)
 for i in range(size):
     sortList[i] = int(sortList[i])
 
-for x in range(size):
+for x in range(size - 1):
     for y in range(size - 1):
         if sortList[y] > sortList[y + 1]:
             temp = sortList[y]
@@ -13,5 +14,5 @@ for x in range(size):
             sortList[y + 1] = temp
 
     for i in range(size):
-        print(str(sortList[i]) + ' ', end='')
+        print(str(sortList[i]), end=' ')
     print()
--- Practice.py
+++ Practice.py
@@ -1,11 +1,19 @@
 __author__ = '보운'
 
-import os
-
-file = os.open("input.txt", os.O_CREAT | os.O_RDWR)
-
-os.dup2(file, 0)
-
-print(input())
-
-os.close(file)
+# T = int(input())
+# for t in range(T):
+#     size = int(input())
+#     n = size
+#     for i in range(size):
+#         if i < size / 2:
+#             n -= 1
+#         else:
+#             n += 1
+#         for j in range(size):
+#             if j < n:
+#                 print(' ', end='')
+#             elif j == 0 or j == n - 1:
+#                 print('*', end='')
+#             else:
+#                 print('+', end='')
+#         print()
