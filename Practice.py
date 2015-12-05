0dccb497078fdc5b1ada8a33dd07a7eae1579393
KimBoWoon <haioer@naver.com>
2015-12-05 22:37:35 +0900

    6

--- Practice.py
+++ Practice.py
@@ -1,19 +1,7 @@
 __author__ = '보운'
 
-T = int(input())
-for t in range(T):
-    size = int(input())
-    n = size
-    for i in range(size):
-        if i < size / 2:
-            n -= 1
-        else:
-            n += 1
-        for j in range(size):
-            if j < n:
-                print(' ', end='')
-            elif j == 0 or j == n - 1:
-                print('*', end='')
-            else:
-                print('+', end='')
-        print()
+a = {'1': [['이름', '주소', '사진1'], ['이름', '주소', '사진2'], ['이름', '주소', '사진2']], '2': [['이름2', '주소2', '사진21']]}
+while True:
+    c = input()
+    for i in range(len(a[c])):
+        print(a[c][i][0], a[c][i][1], a[c][i][2])
